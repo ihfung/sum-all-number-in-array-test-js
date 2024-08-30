aed4261d1a5996ba76c552eaf45fc0ea8519ffe4
ihleung <ihfungleung@gmail.com>
2024-08-30 10:12:05 -0400

    updated as per feedback

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const sumItems = function(array) {
-  // Sum all the numbers in the array
+  // Initialize sum
   let sum = 0;
-  for (let item of array) {
+
+  // Loop through each item in the array
+  for (const item of array) {
+    // If the item is an array, call the function recursively
     if (Array.isArray(item)) {
       sum += sumItems(item);
     } else {
+      // If the item is a number, add it to the sum
       sum += item;
     }
   }
+
+  // Return the total sum
   return sum;
 };
 
